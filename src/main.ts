472a682818ac9b9b9c9cdb3c71c91a5d8e9fd428
BrianDYKim <18k7102dy@naver.com>
2023-02-01 18:09:31 +0900

    docs: swagger 문서화 적용 및 보안설정 완료

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,22 @@ import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
+import * as expressBasicAuth from 'express-basic-auth';
 
 async function bootstrap() {
+  const APPLICATION_NAME: string = process.env.APPLICATION_NAME;
+  const APPLICATION_DESCRIPTION: string = process.env.APPLICATION_DESCRIPTION;
+  const APPLICATION_VERSION: string = process.env.APPLICATION_VERSION;
   const PORT = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
 
   app.useGlobalInterceptors(new LoggingInterceptor());
 
@@ -15,6 +27,28 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  // security for swagger
+  app.use(
+    ['/docs', 'docs-json'],
+    expressBasicAuth({
+      challenge: true,
+      users: { [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD },
+    }),
+  );
+
+  // documentation
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle(APPLICATION_NAME)
+    .setDescription(APPLICATION_DESCRIPTION)
+    .setVersion(APPLICATION_VERSION)
+    .build();
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(
+    app,
+    swaggerConfig,
+  );
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(PORT);
 }
